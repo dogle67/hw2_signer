e33e29fd1ac6fb08dc7a6858ae1b8584c68263d7
dogle67 <lilian.ciobanu1996@gmail.com>
2019-11-06 23:57:16 +0200

    multi hash and combined

--- signer.go
+++ signer.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"sort"
+	"strconv"
+	"sync"
+)
 
 //ExecutePipeline execute piplines
 func ExecutePipeline(jobs ...job) {
@@ -19,9 +23,18 @@ func SingleHash(in, out chan interface{}) {
 func singleHash(out chan interface{}, data string) {
 
 	ch1 := make(chan string)
+	ch2 := make(chan string)
 
 	go crc(ch1, data)
 
+	go mdfive(ch2, data)
+	cm := DataSignerCrc32(<-ch2)
+
+	select {
+	case v := <-ch1:
+		out <- v + "~" + cm
+
+	}
 }
 
 func crc(ch chan string, data string) {
@@ -42,9 +55,45 @@ func mdfive(ch chan string, data string) {
 //MultiHash multi hash
 func MultiHash(in, out chan interface{}) {
 
+	for i := range in {
+		data := i.(string)
+		go multiHash(out, data)
+	}
+}
+
+func multiHash(out chan interface{}, data string) {
+	chanels := make([]chan string, 6)
+	for i := range chanels {
+		chanels[i] = make(chan string)
+		go crc(chanels[i], strconv.FormatInt(int64(i), 10)+data)
+	}
+
+	rsp := ""
+	for i := range chanels {
+		rsp += <-chanels[i]
+	}
+
+	out <- rsp
 }
 
 //CombineResults combine results
 func CombineResults(in, out chan interface{}) {
 
+	strs := make([]string, 0)
+
+	for i := range in {
+		str := i.(string)
+		strs = append(strs, str)
+	}
+
+	sort.Strings(strs)
+	rsp := ""
+	if len(strs) > 0 {
+		rsp = strs[0]
+		for i := 1; i < len(strs); i++ {
+			rsp += "_" + strs[i]
+		}
+	}
+
+	out <- rsp
 }
